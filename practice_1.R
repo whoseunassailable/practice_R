8dd11be5d529cc0c4be3640dfdb499f14771e428
Rohan Bhande <rbhande@hawk.iit.edu>
2024-11-01 20:09:05 -0500

    added_practice_1.R

--- practice_1.R
+++ practice_1.R
@@ -1,3 +1,22 @@
 a <- 10
 b <- 5
 c <- a + b
+c
+
+# variable 1 
+var1 <- 4
+
+# variable 2
+var2 <- 2
+
+# result 
+result <- var1 / var2
+
+result
+
+# Combining two messages in R
+message1 <- "Hello this is"
+name <- "Rohan"
+
+combined_message <- paste(message1, name)
+combined_message
